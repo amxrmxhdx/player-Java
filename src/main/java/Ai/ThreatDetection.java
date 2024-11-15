02d97534461635014bac4d22ad09b2bbb1d99f2c
ahako <dellerwin0@gmai.com>
2024-11-15 23:07:07 +0100

    Added BitsInTurns to ThreatDetection

--- src/main/java/Ai/ThreatDetection.java
+++ src/main/java/Ai/ThreatDetection.java
@@ -1,20 +1,19 @@
 package Ai;
 
 import logic.Strategy;
-import models.BoardAction;
-import models.GameConfig;
-import models.GameState;
-import models.Progress;
+import models.*;
+import utils.Methods;
 
 public class ThreatDetection {
     public static boolean incomingThreat(BoardAction boardAction, GameState gameState) {
+        Base base = Strategy.baseById.get(boardAction.getDest());
         int incomEbits = 0;
         Progress progress = boardAction.getProgress();
-        if (Strategy.baseById.get(boardAction.getDest()).getPlayer() == gameState.getGame().getPlayer()) {
+        if (base.getPlayer() == gameState.getGame().getPlayer()) {
             if (progress.getDistance() > 10) {
                 incomEbits = boardAction.getAmount() - (progress.getDistance() - 10);
             } else incomEbits = boardAction.getAmount();
-            return incomEbits >= Strategy.baseById.get(boardAction.getDest()).getPopulation();
+            return incomEbits >= base.getPopulation() + Methods.BitsInTurns(base, progress.getDistanceLeft(), gameState);
         }
         return false;
     }
