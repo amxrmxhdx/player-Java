055b3f89ad544d514c171b856d260d644def8f17
MickMMars <a.khamisipour@outlook.de>
2024-11-15 22:26:05 +0100

    Fetching bases

--- src/main/java/logic/Strategy.java
+++ src/main/java/logic/Strategy.java
@@ -1,15 +1,44 @@
 package logic;
 
 import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
+import models.Base;
 import models.GameState;
 import models.PlayerAction;
 
 public class Strategy{
-    
+
+    public static Map<Integer, Base> baseById = null;
+
+    public static void fetchBases(List<Base> bases) {
+        baseById = new HashMap<>();
+        for (Base b : bases)
+            baseById.put(b.getUid(), b);
+    }
+
+    public static List<Base> getBasesByPlayer(int player) {
+        List<Base> ret = new ArrayList<>();
+        for (Base b : baseById.values()) {
+            if (b.getPlayer() == player)
+                ret.add(b);
+        }
+        return ret;
+    }
+
+    public static List<Base> getBasesExPlayer(int player) {
+        List<Base> ret = new ArrayList<>();
+        for (Base b : baseById.values()) {
+            if (b.getPlayer() != player)
+                ret.add(b);
+        }
+        return ret;
+    }
+
     public static List<PlayerAction> decide(GameState gameState){
-        //TODO: add your logic here
+        fetchBases(gameState.getBases());
         gameState.getBases().get(0);
         List<PlayerAction> playerAction = new ArrayList<>();
         playerAction.add(new PlayerAction(0, 0, 0));
