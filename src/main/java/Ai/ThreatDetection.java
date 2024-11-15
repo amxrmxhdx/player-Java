65859414e213817fdebde816d371e8db0bc55f18
ahako <dellerwin0@gmai.com>
2024-11-15 23:31:16 +0100

    made ThreatDetection return difference in bits

--- src/main/java/Ai/ThreatDetection.java
+++ src/main/java/Ai/ThreatDetection.java
@@ -1,11 +1,12 @@
 package Ai;
 
+import com.sun.java.swing.action.AboutAction;
 import logic.Strategy;
 import models.*;
 import utils.Methods;
 
 public class ThreatDetection {
-    public static boolean incomingThreat(BoardAction boardAction, GameState gameState) {
+    public static int incomingThreat(BoardAction boardAction, GameState gameState) {
         Base base = Strategy.baseById.get(boardAction.getDest());
         int incomEbits = 0;
         Progress progress = boardAction.getProgress();
@@ -13,8 +14,10 @@ public class ThreatDetection {
             if (progress.getDistance() > 10) {
                 incomEbits = boardAction.getAmount() - (progress.getDistance() - 10);
             } else incomEbits = boardAction.getAmount();
-            return incomEbits >= base.getPopulation() + Methods.BitsInTurns(base, progress.getDistanceLeft(), gameState);
+            if (incomEbits >= (base.getPopulation() + Methods.BitsInTurns(base, progress.getDistanceLeft(), gameState))){
+                return incomEbits - (base.getPopulation() + Methods.BitsInTurns(base, progress.getDistanceLeft(), gameState));
+            }
         }
-        return false;
+        return 0;
     }
 }
